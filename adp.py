b31e26bcead5b98297ee725162018f0b569976b4
sec-ab-it <secabit1337@gmail.com>
2020-03-08 12:06:10 +0100

    Added locking screen on any input.

---
+++ adp.py
@@ -0,0 +1,66 @@
+import ctypes
+import os
+from time import sleep
+from threading import Event
+from pynput import mouse, keyboard
+
+keyboard_controller = keyboard.Controller()
+
+screen_locked = Event()
+
+
+# Mouse handlers
+def on_move(x, y):
+    lock_screen()
+    return False
+
+
+def on_click(x, y, button, pressed):
+    lock_screen()
+    return False
+
+
+def on_scroll(x, y, dx, dy):
+    lock_screen()
+    return False
+
+
+# Keyboard handlers
+def on_press(key):
+    lock_screen()
+    return False
+
+
+def on_release(key):
+    lock_screen()
+    return False
+
+
+def lock_screen():
+    if os.name is 'nt':  # Windows
+        ctypes.windll.user32.LockWorkStation()
+    else:  # Mac Os
+        ctypes.CDLL('/System/Library/PrivateFrameworks/login.framework/Versions/Current/login').SACLockScreenImmediate()
+    screen_locked.set()
+
+
+keyboard_listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+
+mouse_listener = mouse.Listener(
+    on_move=on_move,
+    on_click=on_click,
+    on_scroll=on_scroll)
+
+print("Program will be activated within 5 seconds...")
+sleep(5)
+print("Protection activated!")
+
+keyboard_listener.start()
+mouse_listener.start()
+
+while True:
+    sleep(0.2)
+    if screen_locked.is_set():
+        exit(0)
